93adc37e8958719ef8f7efc9075243e8bd52499d
雨下整夜 <5151644+yuxiazhengye@user.noreply.gitee.com>
2021-09-26 20:05:20 +0800

    add createComponents

---
+++ src/components/Header/index.vue
@@ -0,0 +1,70 @@
+<!--  -->
+<template>
+  <header id="header">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  name: "Header",
+  //import引入的组件需要注入到对象中才能使用
+  props:
+  // ['title',"喵喵电影"],
+   {
+    title: {
+      type: String,
+      default: "喵喵电影",
+    },
+  },
+  components: {},
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
+#header {
+  width: 100%;
+  height: 50px;
+  color: #fff;
+  background: #e54847;
+  border-bottom: 1px solid #e54847;
+  position: relative;
+}
+#header h1 {
+  font-size: 18px;
+  text-align: center;
+  line-height: 50px;
+  font-weight: normal;
+}
+#header i {
+  position: absolute;
+  left: 5px;
+  top: 50%;
+  margin-top: -13px;
+  font-size: 26px;
+}
+</style>
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,81 @@
+<!--  -->
+<template>
+  <div id="main">
+    <Header title="喵喵电影" />
+    <div id="content">
+      <!-- 导航 筛选 -->
+      <div class="movie_menu">
+        <router-link tag="div" to="/movie/city" class="city_name">
+          <span>大连</span>
+          <i class="iconfont icon-lower-triangle"></i>
+        </router-link>
+        <div class="hot_swtich">
+          <router-link tag="div" to="/movie/nowPlaying" class="hot_item">正在热映</router-link>
+          <router-link tag="div" to="/movie/comingSoon" class="hot_item">即将上映</router-link>
+        </div>
+        <router-link tag="div" to="/movie/search" class="search_entry">
+          <i class="iconfont icon-sousuo"></i>
+        </router-link>
+      </div>
+      <!-- 这里做一个二级路由渲染 -->
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
+
+    <TabBar />
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+
+export default {
+  name: "Movie",
+  //import引入的组件需要注入到对象中才能使用
+  components: {
+    Header,
+    TabBar,
+  },
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+#content .movie_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-between; align-items:center; background:white; z-index:10;}
+.movie_menu .city_name{ margin-left: 20px; height:100%; line-height: 45px;}
+.movie_menu .city_name.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .city_name.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .hot_swtich{ display: flex; height:100%; line-height: 45px;}
+.movie_menu .hot_item{ font-size: 15px; color:#666; width:80px; text-align:center; margin:0 12px; font-weight:700;}
+.movie_menu .hot_item.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .hot_item.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry{ margin-right:20px; height:100%; line-height: 45px;}
+.movie_menu .search_entry.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry i{  font-size:24px; color:red;}  
+
+</style>
